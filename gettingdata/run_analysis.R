46838dbaf24081476a3f7599d97833aef3c6c339
Kourosh Razban <krazban@gmail.com>
2014-04-25 15:30:41 +0200

    re-indent the code and cleaning up.

--- gettingdata/run_analysis.R
+++ gettingdata/run_analysis.R
@@ -1,5 +1,6 @@
 # Final Getting and Cleaning Data Project
 # Author: Kourosh Razban
+# File Name: run_analysis.R 
 # ========================================================
 # File download
 
@@ -35,7 +36,7 @@ x.test.dt <- read.table(unz(samsung.data, "UCI HAR Dataset/test/X_test.txt"))
 
 y.test.dt <- read.table(unz(samsung.data, "UCI HAR Dataset/test/y_test.txt"))
 
-# find the activtiy name from activity code.
+# find the activtiy name from activity code key.
 activities <- tolower(activity.lables[[2]][y.test.dt$V1]) 
 
 test.ds <- cbind(subject.ids, activities, y.test.dt, x.test.dt)
@@ -86,23 +87,24 @@ ncol(selected.ds)
 # 79 selected features+ 3(subjectid,activity,activitycode)=82  columns
 #------------------------------------------------------------------- 
 agg.tidy.ds <- aggregate(x= selected.ds[, 4:ncol(selected.ds)],
-                   by= list(subject.id= selected.ds$subject.id,
-                           activity= selected.ds$activity,
-                           activity.code= selected.ds$activity.code), FUN="mean")
+                         by= list(subject.id= selected.ds$subject.id,
+                                  activity= selected.ds$activity,
+                                  activity.code= selected.ds$activity.code), FUN="mean")
 
 # reorder the dataset for each subject.
 agg.tidy.ds <- agg.tidy.ds[order(agg.tidy.ds$subject.id, agg.tidy.ds$activity.code),]
 
 #tidy up the column names
+#-----------------------
+names(agg.tidy.ds)<- gsub("\\.{2}","\\.", 
+                          gsub('([[:upper:]])', '.\\1',                                    
+                               gsub("-", "\\.",     
+                                    gsub("\\(\\)", "", names(agg.tidy.ds)))))
 
-names(agg.tidy.ds)<-tolower(names(agg.tidy.ds))
-
-gsub("-","\\.",
-                gsub("\\(\\)", "", names(agg.tidy.ds), perl=T))
-
+names(agg.tidy.ds) <- tolower(names(agg.tidy.ds))
 nrow(agg.tidy.ds)
 ncol(agg.tidy.ds)
-write.table(agg.tidy.ds,file="data/aggtidyds.txt")
+write.table(agg.tidy.ds,file="data/samsungtidyds.txt")
 
 #--------- END-----------------
 
